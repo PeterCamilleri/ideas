f4a7b0f80854b8f8fbe3b1fe6e4ab61c337319f6
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-02-04 12:44:42 -0500

    Trying out IO/console. No good!

---
+++ io_console/io_console.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+require 'io/console'
+
+puts
+puts "With IO.console"
+puts IO.console.inspect
+puts "IO.console.winsize #does not work!"
+puts
+puts "With STDOUT"
+puts STDOUT.inspect
+puts "winsize = #{STDOUT.winsize.inspect}"
+
+puts
+puts "q to quit."
+
+char = nil
+
+# No special characters, messed up enter keys...
+# Raw input??? Hardly!!! This is most unacceptable!
+
+while char != 'q'
+  STDIN.raw do |inp|
+    print (char = inp.getch).bytes.to_a
+  end
+end
