d75ce82fbbca56db5a7b2daadbb941759fa041f1
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-04-05 10:08:00 -0400

    More String studies.

--- subclass_string/subclass.rb
+++ subclass_string/subclass.rb
@@ -1,37 +1,53 @@
+#A study of sub-classing the String class and the propogation
+#of the frozen state.
 
-
-class Foo < String
+class BahBar < String
 
   def initialize(text)
-    super(text)
+    super(text.to_s.dup)
   end
 
 end
 
-s = "bar"
+s = "bar".freeze
+puts "s is a #{s.class}. Frozen? (#{s.frozen?}) Value: (#{s})"
 
-puts "s (#{s}) is a #{s.class}."
+t = s.dup
+puts "t is a #{t.class}. Frozen? (#{t.frozen?}) Value: (#{t})"
 
-f = Foo.new("foo")
+u = s.clone
+puts "u is a #{u.class}. Frozen? (#{u.frozen?}) Value: (#{u})"
 
-puts "f (#{f}) is a #{f.class}."
+v = s.to_s
+puts "v is a #{v.class}. Frozen? (#{v.frozen?}) Value: (#{v})"
 
-f << "bar"
+x = (s + "abas").freeze
+puts "x is a #{x.class}. Frozen? (#{x.frozen?}) Value: (#{x})"
 
-puts "f (#{f}) is a #{f.class}."
+y = x[1...-1]
+puts "y[1...-1] is a #{y.class}. Frozen? (#{y.frozen?}) Value: (#{y})"
+
+f = BahBar.new(s)
+puts "f is a #{f.class}. Frozen? (#{f.frozen?}) Value: (#{s})"
+
+f << s
+puts "f is a #{f.class}. Frozen? (#{f.frozen?}) Value: (#{f})"
 
 g = f[1...-1]
+puts "g[1...-1] is a #{g.class}. Frozen? (#{g.frozen?}) Value: (#{g})"
 
-puts "g (#{g}) is a #{g.class}."
-
-h = f.to_s
-
-puts "h (#{h}) is a #{h.class}."
+h = f.to_s.freeze
+puts "h is a #{h.class}. Frozen? (#{h.frozen?}) Value: (#{h})"
 
 # Outputs the following:
 
-# s (bar) is a String.
-# f (foo) is a Foo.
-# f (foobar) is a Foo.
-# g (ooba) is a Foo.
-# h (foobar) is a String.
+# s is a String. Frozen? (true) Value: (bar)
+# t is a String. Frozen? (false) Value: (bar)
+# u is a String. Frozen? (true) Value: (bar)
+# v is a String. Frozen? (true) Value: (bar)
+# x is a String. Frozen? (true) Value: (barabas)
+# y[1...-1] is a String. Frozen? (false) Value: (araba)
+# f is a BahBar. Frozen? (false) Value: (bar)
+# f is a BahBar. Frozen? (false) Value: (barbar)
+# g[1...-1] is a BahBar. Frozen? (false) Value: (arba)
+# h is a String. Frozen? (true) Value: (barbar)
