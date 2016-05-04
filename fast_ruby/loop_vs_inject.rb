09b2f5d2b1469646aed84049aaaac8589cd1fea5
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-05-04 11:55:51 -0400

    Added results to loop_vs_inject study.

--- fast_ruby/loop_vs_inject.rb
+++ fast_ruby/loop_vs_inject.rb
@@ -20,3 +20,26 @@ Benchmark.ips do |x|
   x.report("Build string with times inject") { use_times_inject }
   x.compare!
 end
+
+# Tested under: ruby 2.1.6p336 (2015-04-13 revision 50298) [i386-mingw32]
+#
+# Warming up --------------------------------------
+# Build string with times loop
+#                         28.014k i/100ms
+# Build string with range inject
+#                         23.216k i/100ms
+# Build string with times inject
+#                         20.347k i/100ms
+# Calculating -------------------------------------
+# Build string with times loop
+#                         375.149k (± 6.6%) i/s -      1.877M
+# Build string with range inject
+#                         301.915k (± 6.1%) i/s -      1.509M
+# Build string with times inject
+#                         255.246k (± 2.6%) i/s -      1.282M
+#
+# Comparison:
+# Build string with times loop:   375148.6 i/s
+# Build string with range inject:   301914.9 i/s - 1.24x slower
+# Build string with times inject:   255245.9 i/s - 1.47x slower
+
