81eca4f300b19dee66e872b3a97582be99f738db
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-05-05 11:14:45 -0400

    Added rotate right results.

--- fast_ruby/rotate_right.rb
+++ fast_ruby/rotate_right.rb
@@ -17,3 +17,20 @@ Benchmark.ips do |x|
   x.report("Rotate with conditional restore")  { use_condition }
   x.compare!
 end
+
+# Tested under: ruby 2.1.6p336 (2015-04-13 revision 50298) [i386-mingw32]
+#
+# Warming up --------------------------------------
+# Rotate with shift restore
+#                          1.731k i/100ms
+# Rotate with conditional restore
+#                          2.252k i/100ms
+# Calculating -------------------------------------
+# Rotate with shift restore
+#                          17.990k (± 1.3%) i/s -     90.012k
+# Rotate with conditional restore
+#                          23.113k (± 1.1%) i/s -    117.104k
+#
+# Comparison:
+# Rotate with conditional restore:    23112.9 i/s
+# Rotate with shift restore:    17990.0 i/s - 1.28x slower
