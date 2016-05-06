3940ffa67a8ab8f3aff60eaccabd785aff8b96da
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-05-06 11:29:51 -0400

    Some ideas on data scrambling.

---
+++ ginger/scramble.rb
@@ -0,0 +1,50 @@
+#Test scrambling up some data.
+
+require 'pp'
+
+module Scrambler
+
+  def self.scramble(input_string, window, prng, fill_char=' ')
+    @input  = input_string.bytes
+    @window = window
+    @prng   = prng
+
+    @fill   = fill_char.bytes[0]
+
+    @length = @input.length
+    @processed = 0
+
+    do_scramble
+  end
+
+  def self.do_scramble
+    result = []
+
+    while @processed < @length
+      top_up
+
+      index = @prng.rand(@window)
+
+      #result << (@input.delete_at(index) ^ @prng.rand(256))
+      result << (@input.delete_at(index))
+
+      @processed += 1 if (index + @processed) < @length
+    end
+
+    result.pack "C*"
+  end
+
+  def self.top_up
+    while @input.length < @window
+      @input << @fill
+    end
+  end
+
+
+end
+
+pp test_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+pp Scrambler.scramble(test_str, 8, Random.new(1234))
+
+
+
