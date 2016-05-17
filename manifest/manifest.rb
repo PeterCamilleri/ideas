1f43cb7399fb33374e46b4e2adc2aea8fbdfef99
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-05-17 19:57:21 -0400

    Some minor cleanup.

--- manifest/manifest.rb
+++ manifest/manifest.rb
@@ -7,6 +7,8 @@ mods = ObjectSpace.each_object(Module).select {|c| c.const_defined?("VERSION") }
 
 width = (mods.max_by{|m| m.to_s.length}).to_s.length
 
-list = mods.map {|m| "#{m.to_s.ljust(width)} #{m.const_get(:VERSION)}" }
+list = mods.map do |m|
+  "#{m.to_s.ljust(width)} #{m.const_get(:VERSION)}"
+end
 
 puts list.sort
