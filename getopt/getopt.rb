989098661a186618ef6feec2aa6b447a5a000aed
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-14 10:29:00 -0400

    Added typical error handling.

--- getopt/getopt.rb
+++ getopt/getopt.rb
@@ -1,20 +1,35 @@
 require 'getoptlong'
 
+def help
+  puts
+  puts "getopt usage summary:"
+  puts
+  puts "--input,  -i <input data>   # Specify the input data"
+  puts "--output, -o <output data>  # Specify the input data"
+  puts "--key,    -k <key data>     # Specify the key data"
+  puts
+end
+
+
 opts = GetoptLong.new(
   [ "--input",  "-i", GetoptLong::REQUIRED_ARGUMENT ],
   [ "--output", "-o", GetoptLong::REQUIRED_ARGUMENT ],
   [ "--key",    "-k", GetoptLong::REQUIRED_ARGUMENT ])
 
-puts "Option List Study"
+puts "Option List Study version 231.15.6 build: 278,331"
 puts
 
 option_found = false
 
-opts.each do |opt, arg|
-
-  puts "opt = #{opt}, arg = #{arg.inspect}"
-  option_found = true
-
+begin
+  opts.each do |opt, arg|
+    puts "opt = #{opt}, arg = #{arg.inspect}"
+    option_found = true
+  end
+rescue => err
+  puts "Error: #{err.message}"
+  help
+  exit
 end
 
-puts "No options found" unless option_found
+help unless option_found
