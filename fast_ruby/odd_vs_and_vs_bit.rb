a59ade7dfe7a021efca42b0688139265da450348
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-07-05 13:03:31 -0400

    Updated odd study

--- fast_ruby/odd_vs_and_vs_bit.rb
+++ fast_ruby/odd_vs_and_vs_bit.rb
@@ -1,7 +1,7 @@
 require "benchmark/ips"
 
-COUNT = 1_000
-ANSWER = 250500
+COUNT  =   1_000
+ANSWER = 250_500
 
 def use_odd
   result = 0
@@ -15,31 +15,52 @@ def use_and
   fail "Incorrect #{result}" unless result == ANSWER
 end
 
+def use_and_alt
+  result = 0
+  COUNT.times { |i| result += (i&1).zero? ? i : 2 }
+  fail "Incorrect #{result}" unless result == ANSWER
+end
+
 def use_bit
   result = 0
   COUNT.times { |i| result += i[0] == 1 ? 2 : i }
   fail "Incorrect #{result}" unless result == ANSWER
 end
 
+def use_bit_alt
+  result = 0
+  COUNT.times { |i| result += i[0].zero? ? i : 2 }
+  fail "Incorrect #{result}" unless result == ANSWER
+end
+
 Benchmark.ips do |x|
-  x.report("Test with odd?") { use_odd }
-  x.report("Test with and")  { use_and }
-  x.report("Test with bit")  { use_bit }
+  x.report("Test with odd?")    { use_odd }
+  x.report("Test with and")     { use_and }
+  x.report("Test with and alt") { use_and_alt }
+  x.report("Test with bit")     { use_bit }
+  x.report("Test with bit alt") { use_bit_alt }
   x.compare!
 end
 
 # Tested under: ruby 2.1.6p336 (2015-04-13 revision 50298) [i386-mingw32]
 #
 # Warming up --------------------------------------
-#       Test with odd?   930.000  i/100ms
-#        Test with and   827.000  i/100ms
-#        Test with bit   692.000  i/100ms
+#       Test with odd?   876.000  i/100ms
+#        Test with and   837.000  i/100ms
+#    Test with and alt   658.000  i/100ms
+#        Test with bit   699.000  i/100ms
+#    Test with bit alt   585.000  i/100ms
 # Calculating -------------------------------------
-#       Test with odd?      9.270k (± 1.6%) i/s -     46.500k
-#        Test with and      8.107k (± 1.2%) i/s -     41.350k
-#        Test with bit      7.152k (± 1.1%) i/s -     35.984k
+#       Test with odd?      9.339k (± 1.3%) i/s -     47.304k
+#        Test with and      8.509k (± 1.7%) i/s -     42.687k
+#    Test with and alt      6.510k (± 2.0%) i/s -     32.900k
+#        Test with bit      6.951k (± 1.1%) i/s -     34.950k
+#    Test with bit alt      5.963k (± 1.2%) i/s -     29.835k
 #
 # Comparison:
-#       Test with odd?:     9270.4 i/s
-#        Test with and:     8107.0 i/s - 1.14x slower
-#        Test with bit:     7151.5 i/s - 1.30x slower
+#       Test with odd?:     9338.7 i/s
+#        Test with and:     8509.0 i/s - 1.10x slower
+#        Test with bit:     6951.5 i/s - 1.34x slower
+#    Test with and alt:     6509.6 i/s - 1.43x slower
+#    Test with bit alt:     5962.8 i/s - 1.57x slower
+
