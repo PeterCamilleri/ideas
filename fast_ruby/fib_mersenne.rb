4081cd919bae503a99d05335c1458bc45af8882a
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-07-07 12:01:51 -0400

    Added test for fibonacci 1.1.3

--- fast_ruby/fib_mersenne.rb
+++ fast_ruby/fib_mersenne.rb
@@ -53,12 +53,33 @@ end
 # Generator with mersenne:        7.5 i/s
 # Generator with fibonacci:        0.3 i/s - 22.64x slower
 
-# Version 1.1.1 vs 1.1.2
+# Version 1.1.3
+#
+# Warming up --------------------------------------
+# Generator with fibonacci
+#                          1.000  i/100ms
+# Generator with mersenne
+#                          1.000  i/100ms
+# Calculating -------------------------------------
+# Generator with fibonacci
+#                           0.454  (± 0.0%) i/s -      3.000  in   6.613378s
+# Generator with mersenne
+#                           7.611  (± 0.0%) i/s -     39.000
+#
+# Comparison:
+# Generator with mersenne:        7.6 i/s
+# Generator with fibonacci:        0.5 i/s - 16.78x slower
+
+# Version comparison.
 #
 # Generator with fibonacci 1.1.1
 #                           0.293  (± 0.0%) i/s -      2.000  in   6.823390s
 # Generator with fibonacci 1.1.2 (speed_up_one branch)
 #                           0.329  (± 0.0%) i/s -      2.000  in   6.073346s
+# Generator with fibonacci 1.1.3
+#                           0.454  (± 0.0%) i/s -      3.000  in   6.613378s
 #
-# Version 1.1.1 is 1.123 times slower than 1.1.2
+# Version 1.1.1 is the baseline.
+# Version 1.1.2 is 1.22 times faster.
+# Version 1.1.3 is 1.55 times faster.
 #
