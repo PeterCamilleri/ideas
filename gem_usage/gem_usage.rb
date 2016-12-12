e2a564e8be7eaf96a445fcb3d374022f4d765592
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-12-12 16:07:53 -0500

    Updated the gem_usage idea.

--- gem_usage/gem_usage.rb
+++ gem_usage/gem_usage.rb
@@ -1,36 +1,16 @@
 require 'net/https'
 require 'json'
 
-gem_name = "active_record"
+target = ARGV[0]
 
-puts "Gems that use #{gem_name}"
-puts
+if target
+  puts "The gem #{target} is used in:"
 
-def rubygems_get(gem_name: "", endpoint: "")
-  path = File.join("/api/v1/gems/", gem_name, endpoint).chomp("/") + ".json"
-
-  if (read_data = Net::HTTP.get("rubygems.org", path)).empty?
-    puts "No data returned for query of \n #{path}"
-    exit
-  end
-
-  JSON.parse(read_data)
+  url = "https://rubygems.org/api/v1/gems/#{target}/reverse_dependencies.json"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  puts JSON.parse(response).sort
+else
+  puts "Usage: gem_usage <gem_name>"
 end
 
-results = rubygems_get(gem_name: gem_name, endpoint: "reverse_dependencies")
-
-weighted_results = {}
-
-results.each do |name|
-  begin
-    weighted_results[name] = rubygems_get(gem_name: name)["downloads"]
-  rescue => e
-    puts "#{name} #{e.message}"
-  end
-end
-
-weighted_results.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }.first(50).each_with_index do |(k, v), i|
-  puts "#{i}) #{k}: #{v}"
-end
-
-puts "End of list."
