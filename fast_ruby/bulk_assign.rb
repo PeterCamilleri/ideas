90ef3a33197a963bd80a11ebe83d75f43c59384e
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-07-31 20:47:55 -0400

    Updated bulk assignment study with Ruby 2.3.3

--- fast_ruby/bulk_assign.rb
+++ fast_ruby/bulk_assign.rb
@@ -34,3 +34,21 @@ end
 #    Assign from chain:  5172286.9 i/s
 #     Assign from many:  2101560.0 i/s - 2.46x slower
 #    Assign from array:  2096226.3 i/s - 2.47x slower
+
+# Tested under: ruby 2.3.3p222 (2016-11-21 revision 56859) [i386-mingw32]
+#
+# Warming up --------------------------------------
+#     Assign from many    37.382k i/100ms
+#    Assign from array    37.339k i/100ms
+#    Assign from chain    41.627k i/100ms
+# Calculating -------------------------------------
+#     Assign from many      2.215M (± 0.8%) i/s -     11.102M in   5.013041s
+#    Assign from array      2.209M (± 0.7%) i/s -     11.052M in   5.003379s
+#    Assign from chain      4.314M (± 0.5%) i/s -     21.604M in   5.007696s
+#
+# Comparison:
+#    Assign from chain:  4314364.6 i/s
+#     Assign from many:  2214849.0 i/s - 1.95x  slower
+#    Assign from array:  2209068.9 i/s - 1.95x  slower
+
+
