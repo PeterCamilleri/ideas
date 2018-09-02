43898444f295133cf29007c42b9a8484579662bd
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-02 11:47:45 -0400

    More argument ideas.

--- args/key_mixed.rb
+++ args/key_mixed.rb
@@ -4,6 +4,8 @@ def test(*test, keyparm: nil)
   puts keyparm.inspect
 end
 
+puts "Part 1"
+
 test(1, 2, 3)
 
 test(1, 2, 3, keyparm: 42)
@@ -11,3 +13,12 @@ test(1, 2, 3, keyparm: 42)
 test([1, 2, 3])
 
 test([1, 2, 3], keyparm: 42)
+
+puts "Part 2"
+
+def []=(*test, value)
+  puts test.inspect
+  puts value.inspect
+end
+
+self[1,2,3]="A"
