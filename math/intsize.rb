d36bfbe1b14341073628f359571f2fdd9d73be4f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-09 18:27:12 -0400

    Added intsize.rb to explore the transition from Fixnum to Bignum.

---
+++ math/intsize.rb
@@ -0,0 +1,25 @@
+#Find the largest Fixnum.
+
+old = tip = 1
+
+loop do
+
+  old_class   = old.class
+  tip_class   = tip.class
+  tipp1_class = (tip+1).class
+
+  if tip_class == Fixnum && tipp1_class == Bignum
+    break
+  elsif old_class == Bignum
+    old /= 2
+  elsif old_class == Fixnum && tip_class == Fixnum
+    old = tip
+    tip *=2
+  elsif old_class == Fixnum && tip_class == Bignum
+    tip = (tip + old)/2
+  end
+
+  puts "#{tip}, #{old}"
+end
+
+puts "The tipping point is #{tip}"
