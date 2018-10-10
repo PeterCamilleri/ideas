dbe85811e1cad02033a8eedfff606b92d2e7900f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-10 12:38:22 -0400

    Added some more test cases.

--- trace/alt_puts.rb
+++ trace/alt_puts.rb
@@ -15,5 +15,11 @@ ensure
   $stdout = $saved_stdout
 end
 
+puts "Test one - Hello World"
 with_trace { puts "Hello World" }
 
+puts "Test two - [1, 2, 3]"
+with_trace { puts [1, 2, 3] }
+
+puts "Test three - a row of 100 stars"
+with_trace { puts "*"*100 }
