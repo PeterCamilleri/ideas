2c38cfc50107b3c82e2a0972c9956da6fe20640f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-08 21:36:31 -0500

    Improved monkey patch.

--- gem_usage/glmp.rb
+++ gem_usage/glmp.rb
@@ -4,11 +4,9 @@ module Gem
 
   # Originally in  File rubygems.rb at line 816
   def self.latest_spec_for(name)
-    dependency   = Gem::Dependency.new name
+    dependency   = Gem::Dependency.new(name)
     fetcher      = Gem::SpecFetcher.fetcher
-    spec_tuples, = fetcher.spec_for_dependency dependency
-
-    spec_tuples[-1][0]
+    fetcher.spec_for_dependency(dependency)[0][-1][0]
   end
 
 end
