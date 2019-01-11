f9f69fba95bb0266f043c2cc51c19daa95692360
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-11 13:25:54 -0500

    Added ruby 2.3.2 results to looping study.

--- fast_ruby/looping.rb
+++ fast_ruby/looping.rb
@@ -66,3 +66,25 @@ end
 # Looping with indexes:     1297.2 i/s - 1.07x slower
 #     Looping with for:     1291.0 i/s - 1.07x slower
 #    Looping with loop:     1046.1 i/s - 1.33x slower
+
+# Tested under: ruby 2.3.3p222 (2016-11-21 revision 56859) [i386-mingw32]
+#
+# Warming up --------------------------------------
+#   Looping with times   140.000  i/100ms
+#    Looping with each   128.000  i/100ms
+# Looping with indexes   139.000  i/100ms
+#     Looping with for   123.000  i/100ms
+#    Looping with loop   101.000  i/100ms
+# Calculating -------------------------------------
+#   Looping with times      1.418k (± 1.1%) i/s -      7.140k in   5.034850s
+#    Looping with each      1.338k (± 1.3%) i/s -      6.784k in   5.070333s
+# Looping with indexes      1.369k (± 1.0%) i/s -      6.950k in   5.078328s
+#     Looping with for      1.241k (± 0.8%) i/s -      6.273k in   5.054186s
+#    Looping with loop      1.033k (± 0.8%) i/s -      5.252k in   5.085271s
+#
+# Comparison:
+#   Looping with times:     1418.3 i/s
+# Looping with indexes:     1368.7 i/s - 1.04x  slower
+#    Looping with each:     1338.2 i/s - 1.06x  slower
+#     Looping with for:     1241.2 i/s - 1.14x  slower
+#    Looping with loop:     1032.9 i/s - 1.37x  slower
