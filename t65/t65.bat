3106cac59e97b551ab12ccd2f4fb63decb99e909
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-10-10 09:33:50 -0400

    Further refined ideas for testing 6502 code

--- t65/t65.bat
+++ t65/t65.bat
@@ -1,9 +1,9 @@
 @echo off
-echo Test the minimum pass program
+echo Run the test program %1
 
-ca65 --target sim65c02 %1 -o out.o
-ld65 --target sim65c02 --lib sim65c02.lib out.o
-sim65 a.out
+ca65 --target sim65c02 %1 -o test.o
+ld65 --target sim65c02 --lib sim65c02.lib test.o -o test.out
+sim65 test.out
 echo Test status is %ERRORLEVEL%
-rm a.out
-rm *.o
+rm test.out
+rm test.o
